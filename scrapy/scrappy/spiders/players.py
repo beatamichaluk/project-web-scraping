b1338f9a27fbed4c11b0b1755f759519ced2fac2
beatamichaluk <95308516+beatamichaluk@users.noreply.github.com>
2022-05-14 23:20:31 +0200

    Update players.py

--- scrapy/scrappy/spiders/players.py
+++ scrapy/scrappy/spiders/players.py
@@ -1,16 +1,20 @@
+#Libraries
 import scrapy
 import csv
 import pandas as pd
 
+#Creating class for characteristics of each player - name (and surname), number of played matches, number of matches started on the bench and minutes played in all matches
 class Players(scrapy.Item):
     name        = scrapy.Field()
     matches        = scrapy.Field()
     bench        = scrapy.Field()
     minutes        = scrapy.Field()
 
+#This part of the code help to read links from CSV file - Pandas library required    
 df = pd.read_csv('links.csv')
 urls = df['link'].values.tolist()
 
+#This part of a code extract final data from webpages
 class LinksSpider(scrapy.Spider):
     name = 'players'
     allowed_domains = ['http://www.hppn.pl']
